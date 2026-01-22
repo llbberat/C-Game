3ebfe979ee890def56fb7d4bbb1a3cde3d3038a0
llbberat <158774750+llbberat@users.noreply.github.com>
2026-01-22 14:00:18 +0300

    Enhance Hangman game logic and user interface
    Increased the size of the 'secim' array and added a 'can' variable to track remaining attempts. Improved user prompts and adjusted game logic for better gameplay experience.

--- Hangman/hangman.c
+++ Hangman/hangman.c
@@ -6,13 +6,13 @@
 int main() {
     srand(time(NULL));
 
-    int random, i = 0;
-    char secim[10], harf;
+    int random, i = 0, can = 6;
+    char secim[20], harf;
     char kelime[20];
 
     char arraymeyve[10][12] = {"elma","kivi","armut","avakado","ananas","mandalina","portakal","kumkat","incir","alıç"};
     char arrayterim[10][12] = {"zehir","zarf","yüklem","tümleme","kimya","felsefe","antrenör","hipotalamus","miyelin","spor"};
-    char arrayşair[6][20] = {"can yücel","peyami safa","nazım hikmet","turgut uyar","edip cansever","cemal sureya"};
+    char arrayşair[6][20] = {"can yucel","peyami safa","nazim hikmet","turgut uyar","edip cansever","cemal sureya"};
 
     printf("\nOyun turunu seciniz (meyve-sair-terim): ");
     scanf("%s", secim);
@@ -30,45 +30,60 @@ int main() {
         strcpy(kelime, arrayşair[random]);
     }
     else {
-        printf("Geçersiz seçim!\n");
+        printf("Gecersiz secim!\n");
         return 0;
     }
 
-    while (kelime[i] != '\0') {
-        i++;
-    }
-
+    i = strlen(kelime);//kelime uzunluğunu belirliyoruz
     char guess[i + 1];
+
     for (int j = 0; j < i; j++) {
-        if (kelime[j] == ' ') {
-            guess[j] = ' ';
-        } else {
-            guess[j] = '_';
-        }
-        printf("%c", guess[j]);
+        if (kelime[j] == ' ') guess[j] = ' ';
+        else guess[j] = '_';
+        printf("%c ", guess[j]);
     }
     guess[i] = '\0';
 
-
-    while (1) {
+    while (can > 0) {
+        printf("\n\nKalan Can: %d", can);
         printf("\nLutfen harf giriniz: ");
         scanf(" %c", &harf);
 
+        int dogruMu = 0;
         for (int j = 0; j < i; j++) {
             if (kelime[j] == harf) {
-                guess[j] = kelime[j];
+                if (guess[j] == '_') { // Eğer harf daha önce açılmamışsa
+                    guess[j] = kelime[j];
+                    dogruMu = 1;
+                } else {
+                    dogruMu = 2; // Zaten açılmış harf
+                }
             }
         }
 
-        for (int j = 0; j < i; j++) {
-            printf("%c", guess[j]);
+        if (dogruMu == 1) {
+            printf("Dogru tahmin!");
+        } else if (dogruMu == 2) {
+            printf("Bu harfi zaten bildiniz.");
+        } else {
+            can--; // Yanlış tahminde can azalt
+            printf("Yanlis tahmin! Caniniz azaldi.");
         }
-        printf("\n");
+
+        printf("\nKelime: ");
+        for (int j = 0; j < i; j++) {
+            printf("%c ", guess[j]);
+        }
 
         if (strcmp(guess, kelime) == 0) {
-            printf("\nTebrikler!! Kelimeyi buldunuz: %s\n", kelime);
+            printf("\n\nTebrikler!! Kelimeyi buldunuz: %s\n", kelime);
             break;
         }
+    }
+
+    if (can == 0) {
+        printf("\n\nMaalesef caniniz bitti! Kaybettiniz.");
+        printf("\nDogru kelime: %s\n", kelime);
     }
 
     return 0;
